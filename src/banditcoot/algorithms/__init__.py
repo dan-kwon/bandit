2bb54f544b4ee0a81916453132e27c927cbf6f18
dan-kwon <danielkwon02@gmail.com>
2023-07-06 18:37:31 -0700

    fixed __all__ clause in __init__

--- src/banditcoot/algorithms/__init__.py
+++ src/banditcoot/algorithms/__init__.py
@@ -2,9 +2,9 @@ from ._epsilon_greedy import EpsilonGreedy, ind_max, hello_world
 from ._softmax import Softmax, categorical_draw
 
 __all__ = [
-    EpsilonGreedy,
-    Softmax,
-    ind_max,
-    hello_world,
-    categorical_draw
+    'EpsilonGreedy',
+    'Softmax',
+    'ind_max',
+    'hello_world',
+    'categorical_draw'
 ]
